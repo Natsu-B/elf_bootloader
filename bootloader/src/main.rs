7276f501eef269c11713cbf3152223899aa89e68
Natsu-B <natsu.minatomirai@gmail.com>
2025-09-13 13:45:00 +0900

    feat(bootloader): add memory allocator setup logic

--- bootloader/src/main.rs
+++ bootloader/src/main.rs
@@ -3,6 +3,8 @@
 #![no_main]
 #![recursion_limit = "256"]
 
+extern crate alloc;
+
 #[macro_use]
 pub mod print;
 pub mod interfaces;
@@ -11,21 +13,18 @@ use crate::interfaces::pl011::Pl011Uart;
 use crate::interfaces::pl011::UartNum;
 use crate::print::DEBUG_UART;
 use crate::print::NonSyncUnsafeCell;
+use crate::systimer::SystemTimer;
+use alloc::vec::Vec;
 use core::arch::asm;
-use core::arch::global_asm;
-use core::cell::OnceCell;
 use core::ffi::CStr;
 use core::fmt::Write;
-use core::iter::Once;
 use core::ops::ControlFlow;
 use core::panic::PanicInfo;
-use core::ptr::slice_from_raw_parts;
 use core::slice;
+use core::time::Duration;
+use dtb;
 use dtb::DtbParser;
-use dtb::{self};
 use heapless::String;
-use systimer::SystemTimer;
-use tock_registers::debug;
 
 unsafe extern "C" {
     static mut _BSS_START: usize;
@@ -36,14 +35,14 @@ unsafe extern "C" {
 static PL011_UART_ADDR: NonSyncUnsafeCell<usize> = NonSyncUnsafeCell::new(0x900_0000);
 
 #[unsafe(no_mangle)]
-extern "C" fn main(argc: usize, argv: *const *const u8) -> usize {
+extern "C" fn main(argc: usize, argv: *const *const u8) -> ! {
     let args = unsafe { slice::from_raw_parts(argv, argc) };
     let dtb = DtbParser::init(unsafe {
         str_to_usize(CStr::from_ptr(args[0]).to_str().unwrap()).unwrap()
     })
     .unwrap();
     let debug_uart_cell = unsafe { &mut *DEBUG_UART.get() };
-    dtb.find_node(None, Some("arm,pl011"), &mut |(addr, _size)| {
+    dtb.find_node(None, Some("arm,pl011"), &mut |addr, _size| {
         unsafe { *PL011_UART_ADDR.get() = addr };
         debug_uart_cell.set(Pl011Uart::new(addr as *const u32));
         ControlFlow::Break(())
@@ -52,62 +51,46 @@ extern "C" fn main(argc: usize, argv: *const *const u8) -> usize {
     let debug_uart = debug_uart_cell.get_mut().unwrap();
     debug_uart.init(UartNum::Debug, 115200);
     debug_uart.write("debug uart starting...\r\n");
-    let mut s: String<256> = String::new();
-    let mut is_device_name = false;
-    println!("Hello println!!!");
+    let mut systimer = SystemTimer::new();
+    systimer.init();
+    println!("setup allocator");
+    allocator::init();
+    dtb.find_node(Some("memory"), None, &mut |addr, size| {
+        allocator::add_available_region(addr, size).unwrap();
+        ControlFlow::Continue(())
+    });
+    dtb.find_memory_reservation_block(&mut |addr, size| {
+        allocator::add_reserved_region(addr, size).unwrap();
+        ControlFlow::Continue(())
+    });
+    dtb.find_reserved_memory_node(
+        &mut |addr, size| {
+            allocator::add_reserved_region(addr, size).unwrap();
+            ControlFlow::Continue(())
+        },
+        &mut |size, align, alloc_range| -> Result<ControlFlow<()>, ()> {
+            if allocator::allocate_dynamic_reserved_region(size, align, alloc_range)
+                .unwrap()
+                .is_some()
+            {
+                Ok(ControlFlow::Continue(()))
+            } else {
+                Err(())
+            }
+        },
+    );
+    allocator::finalize();
+    println!("allocator setup success!!!");
+    let mut vector_test = Vec::new();
+    let mut i = 0;
     loop {
-        s.clear();
-        println!("Search by device_name(d) or compatible_name(c)?");
-        loop {
-            print!("d/c: ");
-            let char = debug_uart.read_char();
-            debug_uart.write_char(char as char);
-            match char {
-                b'd' => {
-                    is_device_name = true;
-                    break;
-                }
-                b'c' => {
-                    is_device_name = false;
-                    break;
-                }
-                _ => println!("\nSorry, try again."),
-            }
+        vector_test.push(i);
+        println!("{:#?}", vector_test);
+        systimer.wait(Duration::from_secs(1));
+        if vector_test.len() > 10 {
+            vector_test.clear();
         }
-        println!("\nEnter the search string:");
-        loop {
-            let char = debug_uart.read_char();
-            match char {
-                b'\r' | b'\n' => break,
-                08 => {
-                    s.pop();
-                } // backspace
-                x => {
-                    if !x.is_ascii() || x.is_ascii_control() {
-                        continue;
-                    } else {
-                        s.push(x as char).unwrap();
-                    }
-                }
-            }
-            debug_uart.write_char(char as char); // echo back
-        }
-        if is_device_name {
-            println!("\nSearching for device_name: '{}' node...", s);
-        } else {
-            println!("\nSearching for compatible_name: '{}' node...", s);
-        }
-        match dtb.find_node(
-            if is_device_name { Some(&s) } else { None },
-            if is_device_name { None } else { Some(&s) },
-            &mut |(addr, size)| {
-                println!("address: 0x{:X}, size: 0x{:X}", addr, size);
-                ControlFlow::Continue(())
-            },
-        ) {
-            Ok(_) => println!("DTB parsing finished.\nSuccess!!!"),
-            Err(x) => println!("An error occurred while parsing the DTB.\nError: {}", x),
-        }
+        i += 1;
     }
 }
 
