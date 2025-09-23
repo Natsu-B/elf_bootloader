c0261c883b9eb783b4e6af15e8fd813abfb02487
Natsu-B <natsu.minatomirai@gmail.com>
2025-09-24 02:59:55 +0900

    feat(file): add StorageDevice for managing block devices and filesystems

--- file/src/lib.rs
+++ file/src/lib.rs
@@ -1,1 +1,70 @@
 #![no_std]
+
+extern crate alloc;
+use core::cell::OnceCell;
+
+use alloc::boxed::Box;
+use alloc::sync::Arc;
+use block_device::VirtIoBlk;
+use block_device_api::BlockDevice;
+use block_device_api::IoError;
+use filesystem::FileSystemErr;
+use filesystem::PartitionIndex;
+
+pub use filesystem::filesystem::FileHandle;
+pub use filesystem::filesystem::OpenOptions;
+
+pub struct StorageDevice {
+    dev: Arc<dyn BlockDevice>,
+    partition: Box<PartitionIndex>, // TODO PartitionIndex can make smaller
+}
+
+impl StorageDevice {
+    pub fn new_virtio(mmio: usize) -> Result<Self, StorageDeviceErr> {
+        let mut io = VirtIoBlk::new(mmio).map_err(error_from_ioerror)?;
+        io.init().map_err(error_from_ioerror)?;
+        let dev = Arc::new(io);
+        Ok(Self {
+            partition: Box::new(
+                PartitionIndex::new(dev.as_ref()).map_err(error_from_file_system_err)?,
+            ),
+            dev,
+        })
+    }
+
+    // pub fn unmount(&self) -> Result<(), StorageDeviceErr> {
+    //     match Arc::try_unwrap(self.dev) {
+    //         Ok(inner) => {
+    //             todo!();
+    //             Ok(())
+    //         }
+    //         Err(still) => Err(StorageDeviceErr::StillUsed),
+    //     }
+    // }
+
+    pub fn open(
+        &self,
+        partition_idx: u8,
+        path: &str,
+        opts: &OpenOptions,
+    ) -> Result<FileHandle, StorageDeviceErr> {
+        self.partition
+            .open(&self.dev, partition_idx, path, opts)
+            .map_err(error_from_file_system_err)
+    }
+}
+
+#[derive(Debug)]
+pub enum StorageDeviceErr {
+    IoErr(IoError),
+    FileSystemErr(FileSystemErr),
+    StillUsed,
+}
+
+fn error_from_ioerror(err: IoError) -> StorageDeviceErr {
+    StorageDeviceErr::IoErr(err)
+}
+
+fn error_from_file_system_err(err: FileSystemErr) -> StorageDeviceErr {
+    StorageDeviceErr::FileSystemErr(err)
+}
